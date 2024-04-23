9af4b112b582fa6fbbd7df156b40878de1de7818
Sanket-825 <sanketparab59@gmail.com>
2024-04-23 15:59:58 +0530

    Init

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectToDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://Thunder825:Thunder825@cluster0.7g1zkxm.mongodb.net/"
+      "mongodb+srv://Thunder825:Thunder825@cluster0.5vreyb6.mongodb.net/"
     );
 
     console.log("mongodb is connected");
